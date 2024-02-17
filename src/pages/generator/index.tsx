1113ff2f672611f711cddd18f94abcaa2b88ae3c
Anastasiia Shapovalova <nastikue@gmail.com>
2024-02-17 05:35:45 +0200

    Update Home page, add other pages placeholders

---
+++ src/pages/generator/index.tsx
@@ -0,0 +1,27 @@
+const GeneratorPage=() => {
+  return <div>
+    <h1>Report generator</h1>
+
+
+
+    <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <a
+        href="/result"
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        rel="noopener noreferrer"
+      >
+        <h2 className={`mb-3 text-2xl font-semibold`}>
+          Generate{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          Please check all fields before clicking!
+        </p>
+      </a>
+    </div>
+  </div>
+};
+
+export default GeneratorPage;
