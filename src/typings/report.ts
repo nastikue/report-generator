74f7d7be8d60b11ebc9bcf5656693fafd4e6e433
Anastasiia <nastikue@gmail.com>
2024-02-19 17:43:19 +0200

    Develop (#5)
    * Feature/update home page (#2)
    
    * Init React + Next project
    
    * Update Home page, add other pages placeholders
    
    * Fix Error: Do not use an  element to navigate to local path (#4)
    
    * Add generator form fields (#6)
    
    * Init React + Next project
    
    * [WIP] Add inputs to Report form
    
    * Fix build errors
    
    * Feature/add code quality checks (#7)
    
    * Init React + Next project
    
    * Add prettier
    
    * Testing pre-commit hook
    
    * Update pre-commit hook: add linter and prettier
    
    * Add prettier config, fix format in all files

---
+++ src/typings/report.ts
@@ -0,0 +1,27 @@
+import { Role } from './enum';
+import { PersonName, Technology } from './types';
+
+interface IRoleDescription extends PersonName {
+    roleName: Role;
+    responsibilities: string;
+    result: string;
+}
+
+export interface IReport {
+    projectName: string;
+    mentor: PersonName;
+    repository: string;
+    design: string;
+    jira: string;
+    sprintsReports: string;
+    app: string;
+    shortProjectDescription: string;
+    technologies: {
+        [key: string]: Technology;
+    };
+    introduction: string;
+    rolesDescription: {
+        [key: string]: IRoleDescription;
+    };
+    additions: string;
+}
