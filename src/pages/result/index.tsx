f44032ade322407e0e1d7de664b40f85428faf5d
Anastasiia Shapovalova <nastikue@gmail.com>
2024-02-19 17:25:32 +0200

    Add prettier config, fix format in all files

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,44 +1,47 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-const ResultPage=({report}: {report: string}) => {
-  const copyToClipboard=() => {
-    navigator.clipboard.writeText(report);
-  }
-  return <div>
-    <h1>Well done!</h1>
-    <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-      <Link
-        href="/generator"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        rel="noopener noreferrer"
-      >
-        <h2 className={`mb-3 text-2xl font-semibold`}>
-          Generate Another Report{" "}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h2>
-        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          Do you want to generate another Report?
-        </p>
-      </Link>
-      <Link
-        href="/"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        rel="noopener noreferrer"
-      >
-        <h2 className={`mb-3 text-2xl font-semibold`}>
-          Back to Home page{" "}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h2>
-        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          Please share your feedback with the dev team :-{")"} Have a great day!
-        </p>
-      </Link>
-    </div>
-  </div>
+const ResultPage = ({ report }: { report: string }) => {
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(report);
+    };
+    return (
+        <div>
+            <h1>Well done!</h1>
+            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+                <Link
+                    href="/generator"
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className={`mb-3 text-2xl font-semibold`}>
+                        Generate Another Report{' '}
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                            -&gt;
+                        </span>
+                    </h2>
+                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                        Do you want to generate another Report?
+                    </p>
+                </Link>
+                <Link
+                    href="/"
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className={`mb-3 text-2xl font-semibold`}>
+                        Back to Home page{' '}
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                            -&gt;
+                        </span>
+                    </h2>
+                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                        Please share your feedback with the dev team :-{')'}{' '}
+                        Have a great day!
+                    </p>
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default ResultPage;
